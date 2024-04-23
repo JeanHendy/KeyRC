ddac37d2c42bca5dc0e5e7a08941f56a75417459
Jean Hendy <jeanyikikh@gmail.com>
2024-04-23 18:04:50 +0200

    First commit from backend branch

---
+++ Cloud-dev/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const crypto = require("crypto");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur KeyRC Cloud Server!");
+});
+
+app.post("/generate-token", (req, res) => {
+  const token = generateToken();
+  res.json({ token: token });
+});
+
+function generateToken() {
+  const token = crypto.randomBytes(16).toString("hex");
+  return token;
+}
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Serveur démarré sur http://0.0.0.0:${port}`);
+});
